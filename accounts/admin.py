0109fb8bbd86df585f99ea1d568d90032d67d78b
Ahmed Ezzt <124591295+ahmed3zzt@users.noreply.github.com>
2023-05-23 14:01:26 +0300

    Create Profile Model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("name",'who_i','price')
+
+
+# admin.site.register(Profile , ProfileAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
+    name = models.CharField(_("الاسم :"), max_length=50) 
+    who_i = models.TextField(_("من انا :"), max_length=250)
+    price = models.IntegerField(_("السعر : "))
+    class Meta:
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profiles') 
+
+
+    def __str__(self):
+        return self.name
